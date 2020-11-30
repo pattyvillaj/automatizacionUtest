688ab9e8303b2521ec214120e8ef402815ce3f98
CHCORP\gvillamizarj <patriciavillamizarj@gmail.com>
2020-11-30 16:43:25 -0500

    Subir proyecto

--- src/main/java/co/com/choucair/certification/automatizacionUtest/userinterface/UtestPage.java
+++
@@ -1,8 +0,0 @@
-package co.com.choucair.certification.automatizacionUtest.userinterface;
-
-import net.serenitybdd.core.pages.PageObject;
-import net.thucydides.core.annotations.DefaultUrl;
-
-@DefaultUrl("https://www.utest.com/")
-public class UtestPage extends PageObject {
-}
